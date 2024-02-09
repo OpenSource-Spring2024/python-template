ff1ff71163b6058b54980dbd46e0994cc16cc73e
Savitha Nair <savnair36@gmail.com>
2024-02-09 17:44:19 -0500

    ruff formatting

--- src/hw1/calculator.py
+++ src/hw1/calculator.py
@@ -5,16 +5,16 @@ def calculator() -> None:
     num1 = float(input("Enter the first number: "))
     operation = input("Enter the operation (+, -, *, /): ")
     num2 = float(input("Enter the second number: "))
-    if operation == '+':
-        result: float = add(num1, num2)
+    if operation == "+":
+        result = add(num1, num2)
         print(f"The result of {num1} + {num2} is {result}")
-    elif operation == '-':
+    elif operation == "-":
         result = subtract(num1, num2)
         print(f"The result of {num1} - {num2} is {result}")
-    if operation == '*':
-        result: float = multiply(num1, num2)
+    if operation == "*":
+        result = multiply(num1, num2)
         print(f"The result of {num1} * {num2} is {result}")
-    elif operation == '/':
+    elif operation == "/":
         result = divide(num1, num2)
         print(f"The result of {num1} / {num2} is {result}")
     else:

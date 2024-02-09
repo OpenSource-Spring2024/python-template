f043fa73666c366003f7036617241bd1a20aed26
jeevanv26 <82412374+jeevanv26@users.noreply.github.com>
2024-02-09 17:57:26 -0500

    Update test file

--- tests/test_1.py
+++ tests/test_1.py
@@ -1,13 +1,8 @@
 # test_google.py
 
-import unittest
-
-class Test1(unittest.TestCase):
-
-    def test_standard_date(self):
-        print("Hello World")
-        
-        assert 2 + 2 == 4
+def test_main(self):
+    print("Hello World")
+    assert 2 + 2 == 4
 
 if __name__ == '__main__':
-    unittest.main()
+    test_main()

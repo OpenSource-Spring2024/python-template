7c36338570a59a10aecc38a337d763ce1c287c87
Jimmy Shong <jimmysh341@gmail.com>
2024-02-09 19:25:53 -0500

    suggestion from TA

--- src/hw1/calculator.py
+++ src/hw1/calculator.py
@@ -1,21 +1,12 @@
-from backend.api import add, divide, multiply, subtract
+from backend.api import add
 
 
-def calculator() -> None:
+def sum() -> None:
     num1 = float(input("Enter the first number: "))
-    operation = input("Enter the operation (+, -, *, /): ")
     num2 = float(input("Enter the second number: "))
-    if operation == "+":
-        result = add(num1, num2)
-        print(f"The result of {num1} + {num2} is {result}")
-    elif operation == "-":
-        result = subtract(num1, num2)
-        print(f"The result of {num1} - {num2} is {result}")
-    if operation == "*":
-        result = multiply(num1, num2)
-        print(f"The result of {num1} * {num2} is {result}")
-    elif operation == "/":
-        result = divide(num1, num2)
-        print(f"The result of {num1} / {num2} is {result}")
-    else:
-        print("Invalid operation. Please try again.")
+    result = add(num1, num2)
+    print(f"The result of {num1} + {num2} is {result}")
+
+
+if __name__ == "__main__":
+    sum()
--- tests/test_1.py
+++ tests/test_1.py
@@ -3,7 +3,3 @@
 
 def test_main() -> None:
     assert 2 + 2 == 4
-
-
-if __name__ == "__main__":
-    test_main()
